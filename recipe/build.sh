d6a422f82f8c0826cf80bc8fb9081a358093c566
xkong-anaconda <xkong@anaconda.com>
2026-07-01 02:06:31 -0700

    Drop -Werror from gn bootstrap (conda GCC -Werror=comment on Linux)

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,10 @@ if [[ "$(uname)" == "Darwin" ]]; then
     sed -i.bak "s/-mmacosx-version-min=14/-mmacosx-version-min=11.0/" build/gen.py
 fi
 
+# Drop -Werror from the bootstrap: conda's GCC on Linux flags a benign multi-line
+# comment in the gn source (-Werror=comment) that upstream's clang build doesn't.
+sed -i.werror.bak "s/cflags.append('-Werror')/pass  # -Werror dropped for conda GCC/" build/gen.py
+
 python build/gen.py
 ninja -C out
 mkdir -p $PREFIX/bin
