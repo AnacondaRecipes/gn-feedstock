4b0fbe484a23af92642ed6b5dc33604b811e83b1
vkhomits <khomitsvalia@gmail.com>
2026-09-10 13:56:47 +0300

    Rebuild for windows arm64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,10 @@
 @echo on
 
+if "%target_platform%"=="win-arm64" (
+    set "GN_MSVC_MACHINE=ARM64"
+) else (
+    set "GN_MSVC_MACHINE=x64"
+)
 REM gn has no separate Windows build script differences; mirror the unix bootstrap.
 python build/gen.py
 if errorlevel 1 exit 1
